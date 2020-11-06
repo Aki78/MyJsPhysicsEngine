8e61b99178da81b535d81c9e484e317c28c20c45
Aki78 <kmorooka78@gmail.com>
2020-11-06 13:13:02 +0200

    on shape circle

--- phys2d.js
+++ phys2d.js
@@ -12,7 +12,7 @@ Vec.prototype.cross = function (v) {return this.x*v.y - v.x*this.y;}
 
 // self translation
 Vec.prototype.move = function (dx, dy) {this.x += dx; this.y += dy;}
-
+//===========================================================================
 //Rectangle
 function ReactangleEntity(x, y, width, height){
   this.shape = ShapeRectangle; this.type = BodyStatic;
@@ -21,5 +21,46 @@ function ReactangleEntity(x, y, width, height){
 
   this.isHit = function(i, j){
     return (this.xx <= i && i<= this.x + this.w && this.y <= j && this.y + this.h)
+  }
+}
+//Line
+function LineEntity(x0,y0,x1,y1, restitution){
+  this.shape = ShapeLine; this.type = BodyStatic;
+  this.x = (x0 + x1)/2; this.y = (y0 + y1)/2;
+  this.x0=x0; this.y0=y0; this.x1=x1; this.y1=y1;
+
+  this.restitution = restitution || 0.9; // ???
+  this.vec = new Vec(x1 - x0, y1 - y0);
+  var length = Math.sqrt(Math.pow(this.vec.x, 2) + Math.pow(this.vec.y, 2));
+  this.norm = new Vec(y0 - y1, x1 - x0).mul(1 / length);
+}
+//===========================================================================
+//Circle
+function CircleEntity(x, y, radius, type, restitution, deceleration) {
+  this.shape = ShapeCirle;
+  this.type = type  || BodyDynamic;
+  this.x = x; this.y = y; this.radius = radius;
+  this.restitution = restitution || 0.9; this.deceleration = deceleration || 1.0; 
+  this.accel = new Vec(0,0); this.velocity = new Vec(0,0);
+
+  this.move = function (dx, dy){ this.x += dx; this.y += dy; }
+  this.isHit = function (x, y){return Math.pow(x - this.x, 2) + Math.pow(y - this.y, 2) < Math.pow(this.radius, 2);}
+
+  this.collidedWithRect = function(r) {
+    var nx = Math.max(r.x, Math.min(this.x, r.x + r.w)); var ny = Math.max(r.y, Math.min(this.y, r.y + r.h));
+    if(!this.isHit(nx,ny)){ return;} // return nothing
+//??????????    if(this.onhit(nx,ny)){ return;} // return nothing ?????????????????
+    if(this.onhit){ return this.onhit(this.r);} // return nothing ?????????????????
+
+    var d2 = Math.pow(nx - this.x, 2) + Math.pow(ny - this.y, 2);
+    var overlap = Math.abs(this.radius - Math.sqrt(d2));
+    var mx=0, my=0;
+
+    if(ny==r.y){my=-overlap;}else if(ny==r.y+r.h){my=overlap;}else if(nx==r.x){mx=-overlap}else if(nx==r.x+r.w){mx=overlap}
+        else{mx=-this.velocity.x;my=-this.velocity.y;}
+
+    this.move(mx,my);
+    if(mx){this.velocity = this.velocity.mul(-1*this.restitution, 1);}
+    if(my){this.velocity = this.velocity.mul(1,-1*this.restitution);}
   }
 }
